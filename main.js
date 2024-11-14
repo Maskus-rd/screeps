776a8526c9b67bac05254d3f7ee8aabd54343b44
Ryan <80864263+Maskus-rd@users.noreply.github.com>
2024-11-13 19:55:30 -0500

    Update main.js
    update to 1.0

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var roleFarmer = require('role.farmer');
 var roleMaintenance = require('role.maintenance');
+var roleBuilder = require('role.builder');
+var autoSpawn = require('auto.spawn');
 
 module.exports.loop = function () {
-
-  for(var name in Game.creeps){
+    
+    autoSpawn.run();
+    
+         
+    for(var name in Game.creeps){
+    
     var creep = Game.creeps[name];
     if(creep.memory.role == 'farmer'){
       roleFarmer.run(creep);
     }
+    
+    
     if(creep.memory.role == 'maintenance'){
       roleMaintenance.run(creep);
     }
+    
+    if(creep.memory.role == 'builder'){
+        roleBuilder.run(creep);
+    }
+    
   }
 }
