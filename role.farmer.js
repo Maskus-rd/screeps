007124533dc42bd37e7a8a735803b299d05bb8ab
Ryan <80864263+Maskus-rd@users.noreply.github.com>
2024-11-13 19:57:42 -0500

    Update role.farmer.js
    update to 1.0

--- role.farmer.js
+++ role.farmer.js
@@ -1,21 +1,28 @@
 var roleFarmer = {
   run: function(creep){
+    
         if(creep.carry.energy == 0){
           creep.memory.farming = true;
         }
+    
         if(creep.carry.energy == creep.store.getCapacity()){
           creep.memory.farming = false;
         }
+      
         if(creep.memory.farming){
           var sources = creep.room.find(FIND_SOURCES);
+          
               if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE){
                 creep.moveTo(sources[0]);
               }
-        }else{  
+             
+        }else{
+            
             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
                 creep.moveTo(Game.spawns['Spawn1']);
             }
          }
+    
   }
 };
 
